bee60fbcd5f7ed90648cf322558d6fa2e54a4b40
Yefim Savin <savin.efim2@gmail.com>
2021-07-21 18:22:33 +0500

    change binding of classes

--- src/main/koschei/models/Egg6.java
+++ src/main/koschei/models/Egg6.java
@@ -8,13 +8,13 @@ public class Egg6 {
 
     private Needle7 needle;
 
-    @Autowired
-    public Egg6(Needle7 needle) {
-        this.needle = needle;
-    }
-
     @Override
     public String toString() {
         return ", в яйце иголка " + needle.toString();
+    }
+
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
     }
 }
